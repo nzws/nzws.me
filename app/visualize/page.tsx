72dd4e75ad7f78d6a988ac7b94d13a70a07bbcc2
Yuzuki Aida <dev@nzws.me>
2023-03-21 00:50:11 +0900

    Fix baseURL

--- app/visualize/page.tsx
+++ app/visualize/page.tsx
@@ -1,6 +1,7 @@
 // @ts-expect-error: why you don't have types?
 import Visualizer from 'next-route-visualizer';
+import { PUBLIC_URL } from '~/utils/constants';
 
 export default function Page() {
-  return <Visualizer />;
+  return <Visualizer baseURL={PUBLIC_URL || undefined} />;
 }
