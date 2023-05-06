e983cabb3ea605f2ded9aff786aaf4e018b5312a
Yuzuki Aida <dev@nzws.me>
2023-05-07 02:34:21 +0900

    Use title font as gen-ei-late-min

--- styles/font.ts
+++ styles/font.ts
@@ -1,4 +1,5 @@
 import { Shippori_Mincho, IBM_Plex_Sans_JP } from 'next/font/google';
+import localFont from 'next/font/local';
 
 export const Sans = IBM_Plex_Sans_JP({
   weight: ['400', '600'],
@@ -12,4 +13,15 @@ export const Mincho = Shippori_Mincho({
   display: 'swap',
   subsets: ['latin-ext'],
   variable: '--font-mincho'
+});
+
+export const GenEiLateMin = localFont({
+  src: [
+    {
+      path: '../assets/GenEiLateMinP_v2.ttf',
+      weight: '400',
+      style: 'normal'
+    }
+  ],
+  variable: '--font-gen-ei-late-min'
 });
