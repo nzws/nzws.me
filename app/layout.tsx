83ee6c7240efbf7eb3e25567cd1e9bf7a2e3b6f6
Yuzuki Aida <dev@nzws.me>
2023-05-07 02:33:42 +0900

    Remove FOUC workaround

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,18 +2,18 @@ import 'ress/dist/ress.min.css';
 import '~/styles/global.scss';
 import { PropsWithChildren } from 'react';
 import { Analytics } from '@vercel/analytics/react';
-import { Sans, Mincho } from '~/styles/font';
+import { Sans, Mincho, GenEiLateMin } from '~/styles/font';
 
-const coreStyleClass = [Sans.variable, Mincho.variable].join(' ');
+const coreStyleClass = [
+  Sans.variable,
+  Mincho.variable,
+  GenEiLateMin.variable
+].join(' ');
 
 export default function RootLayout({ children }: PropsWithChildren) {
   return (
     <html lang="ja" className={coreStyleClass}>
       <body>
-        {/* Workaround for CSS Module's FOUC (needs 1 space) */}
-        {/* ref: https://zenn.dev/catnose99/articles/3c106c81cbfdec */}
-        <script> </script>
-
         {children}
 
         <Analytics />
